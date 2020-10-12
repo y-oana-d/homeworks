b1685c69ac3c5d303abc1070e76db1a60bf7a00f
y-oana-d <67188583+y-oana-d@users.noreply.github.com>
2020-10-12 11:38:19 +0100

    Update sentence_reverse.py

--- sentence_reverse.py
+++ sentence_reverse.py
@@ -4,17 +4,14 @@ class rearrange_sentence:
         self.sentence = None
 
     def sentence_change(self):
-        # get a string from the console
+        
         sentence = input('Please type your sentence here\n>>>')
 
-        # split the string into a list
         new_sentence = sentence.split()
 
-        # reverse the list
+       
         new_sentence.reverse()
 
-
-        # print all the list items using print (*)
         print(*new_sentence)
 
 newclass = rearrange_sentence()
